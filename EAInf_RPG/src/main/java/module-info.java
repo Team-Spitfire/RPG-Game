05dba6f9048dee7d6388da65c44a82e40a8d6785
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2020-10-20 17:29:28 +0200

    Homescreen
    Homescreen knöpfe wurden ersteelt aber ohne funktion

--- EAInf_RPG/src/main/java/module-info.java
+++ EAInf_RPG/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.eainf_rpg {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.eainf_rpg to javafx.fxml;
     exports com.mycompany.eainf_rpg;
