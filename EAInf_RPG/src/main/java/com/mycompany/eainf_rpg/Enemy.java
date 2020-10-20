a937ddb95259c34622a8357dfe24b7c5bee01ab5
BenMajestix <ben.bartel@gymhum.de>
2020-10-20 16:46:32 +0200

    Enemy
    enemy stuff, variables

--- EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/Enemy.java
+++ EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/Enemy.java
@@ -10,5 +10,56 @@ package com.mycompany.eainf_rpg;
  * @author benbartel
  */
 public class Enemy {
+    public String name;
+    public int maxHp;
+    public int currHp;
+    public Weapon weapon;
+    public Armor armor;
+
+    public Enemy() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxHp() {
+        return maxHp;
+    }
+
+    public int getCurrHp() {
+        return currHp;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public Armor getArmor() {
+        return armor;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMaxHp(int maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public void setCurrHp(int currHp) {
+        this.currHp = currHp;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void setArmor(Armor armor) {
+        this.armor = armor;
+    }
     
+    
+           
 }
