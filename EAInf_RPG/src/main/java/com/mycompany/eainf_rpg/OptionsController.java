4c41fdc459179c5e54bc1698536d799452cc7c30
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2020-10-20 17:34:54 +0200

    Options update
    tips & tricks checkbox implimentiert

---
+++ EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/OptionsController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.eainf_rpg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author hanne
+ */
+public class OptionsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void checkTipsTricks(ActionEvent event) {
+    }
+    
+}
