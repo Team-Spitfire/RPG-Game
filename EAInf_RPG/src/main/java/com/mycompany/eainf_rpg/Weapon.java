17d56b52f1956f23c007a65c6048941843dd0a05
XSilentstorm <47358890+XSilentstorm@users.noreply.github.com>
2020-10-20 16:56:51 +0200

    Weapon stats
    Weapon stats sind festgelegt

--- EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/Weapon.java
+++ EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/Weapon.java
@@ -4,14 +4,21 @@
  * and open the template in the editor.
  */
 package com.mycompany.eainf_rpg;
+
 import java.util.*;
+
 /**
  *
  * @author benbartel
  */
-public class Weapon() {
+public class Weapon {
+
     public String name;
-}
-public Weapon(String name) {
-    this.name = name;
+    public int atk;
+
+    public Weapon(String name, int atk) {
+        this.name = name;
+        this.atk = atk;
+    }
+
 }
