d100ab19dd2f0523b7e10424102edac5058f87c7
Pantherr03 <paul.christoph.kern@gmail.com>
2020-10-29 10:40:51 +0100

    unused imports + wrong code removed

--- EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/HomescreenController.java
+++ EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/HomescreenController.java
@@ -5,16 +5,12 @@
  */
 package com.mycompany.eainf_rpg;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
