b398ca76dae798107657e3d3866185a16d18ab6e
BenMajestix <ben.bartel@gymhum.de>
2020-11-12 11:19:54 +0100

    mine is bad

--- EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/Store_ActiveController.java
+++ EAInf_RPG/src/main/java/com/mycompany/eainf_rpg/Store_ActiveController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -35,6 +36,18 @@ public class Store_ActiveController implements Initializable {
     private Button shopPotionLvl1;
     @FXML
     private Label goldCoinValue;
+    @FXML
+    private ImageView weaponLvl3;
+    @FXML
+    private ImageView weapomLvl2;
+    @FXML
+    private ImageView armorLvl3;
+    @FXML
+    private ImageView armorLvl2;
+    @FXML
+    private ImageView potionLvl3;
+    @FXML
+    private ImageView potionLvl2;
         
 
     /**
@@ -49,37 +62,45 @@ public class Store_ActiveController implements Initializable {
     private void buyWeaponLvl2(ActionEvent event) {
         //GameController.upgradeWeapon();
         App.getPlayer().getWeapon().setRare(3);
+        shopWeaponLvl2.setDisable(true);
     }
 
     @FXML
     private void buyWeaponLvl1(ActionEvent event) {
         //GameController.upgradeWeapon();
         App.getPlayer().getWeapon().setRare(2);
+        weaponLvl3.toFront();
         shopWeaponLvl2.toFront();
+        
     }
 
     @FXML
     private void buyArmorLvl2(ActionEvent event) {
         //GameController.upgradeArmor();
         App.getPlayer().getArmor().setRare(3);
+        shopArmorLvl2.setDisable(true);
     }
 
     @FXML
     private void buyArmorLvl1(ActionEvent event) {
         //GameController.upgradeArmor();
         App.getPlayer().getArmor().setRare(2);
+        armorLvl3.toFront();
         shopArmorLvl2.toFront();
     }
 
     @FXML
     private void buyPotionLvl2(ActionEvent event) {
         App.getPlayer().getPotion().setRare(3);
+        shopPotionLvl2.setDisable(true);
     }
 
     @FXML
     private void buyPotionLvl1(ActionEvent event) {
         App.getPlayer().getPotion().setRare(2);
+        potionLvl3.toFront();
         shopPotionLvl2.toFront();
+        App.getPlayer().setGoldCoins(App.getPlayer().getGoldCoins() - 7);
     }
 
     @FXML
@@ -88,7 +109,6 @@ public class Store_ActiveController implements Initializable {
         
     }
     
-    @FXML
     private void adjustCoinValue(){
         goldCoinValue.setText(Integer.toString(App.getPlayer().getGoldCoins()));
     }
